abdd1dda0983ca89803c4caaa488738771234e57
dogefield <120469514+dogefield@users.noreply.github.com>
2025-06-11 15:15:51 -0400

    Add frontend project and API server

---
+++ crypto-backtest-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CryptoBacktestDashboard from './components/CryptoBacktestDashboard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CryptoBacktestDashboard />
+    </div>
+  );
+}
+
+export default App;
